595a92a09a1e89148169746d5fed6d52d051634b
Carl Borgas <carl.borgas@catch.com.au>
2023-09-14 08:59:15 +1000

    Setup project

---
+++ tests/game.test.ts
@@ -0,0 +1,11 @@
+import { GameRunner } from '../src/game-runner';
+
+describe('The test environment', () => {
+  it('should pass', () => {
+    expect(true).toBe(true);
+  });
+
+  it('should access game', () => {
+    expect(GameRunner).toBeDefined();
+  });
+});
